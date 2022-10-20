ce9ef7ca1d8be93e6b0ebb44fcb28fb2b5ad2f80
riley soloner <rileysoloner@gmail.com>
2022-10-20 12:40:55 -0400

    css start your engines

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1> Blank Page</h1>
+      <h1>Blank Page</h1>
+      <h2>This is going to be the best portfolio site I've ever made.</h2>
     </div>
   );
 }
