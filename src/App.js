47c4a693de1a8fc1cd55f8d97566e55a30690f78
riley soloner <rileysoloner@gmail.com>
2022-10-21 18:00:30 -0400

    navbar 1st draft, favicon, and title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import React, { useEffect } from 'react';
 import './App.css';
+import { Navbar, Nav} from 'rsuite'
+import HomeIcon from '@rsuite/icons/legacy/Home';
+
+const Example = () => {
+  useEffect(() => {
+    document.title = 'Riley Soloner';
+  });
+}
+
+
 
 function App() {
+  Example()
   return (
     <div className="App">
-      <h1>Blank Page</h1>
-      <h2>This is going to be the best portfolio site I've ever made.</h2>
+      <Navbar>
+        <Navbar.Brand href='#'>Riley Soloner</Navbar.Brand>
+        <Nav>
+          <Nav.Item icon={ <HomeIcon /> }>Projects</Nav.Item>
+          <Nav.Item>Technologies</Nav.Item>
+          <Nav.Item>About</Nav.Item>
+          <Nav.Item>Contact</Nav.Item>
+        </Nav>
+        <Nav pullRight>
+          <ion-icon href="https://github.com/robotriley/" name="logo-github" size="large"></ion-icon>
+          <ion-icon href="https://https://www.linkedin.com/in/riley-soloner/" name="logo-linkedin" size="large"></ion-icon>
+          <ion-icon href="https://https://www.medium.com/@rileysoloner/" name="logo-medium" size="large"></ion-icon>
+        </Nav>
+      </Navbar>
     </div>
   );
 }
