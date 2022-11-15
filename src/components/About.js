e2d001d1c7f99f0fd4052d96addff61676b078f0
riley soloner <rileysoloner@gmail.com>
2022-11-15 17:21:37 -0500

    well would you look at that hover link at the top of the page

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,10 @@ import '../App.css';
 const About = () => {
   return (
     <div>
-      <p className='para'>I'm a Full Stack Software Engineer! With a background in acting, writing and producing live and televised comedy for ten years, I am used to working in collaborative environments, embracing change and communicating clearly. I’m excited to combine my skills and experience with software engineering to help creative people make more and better art.</p>
+      <p className='para'>I'm a Full Stack Software Engineer! With a background in live/televised comedy,
+       I am used to working in collaborative environments and communicating clearly. 
+       I’m excited to combine my skills and experience with software engineering to help creative 
+       people make more and better art.</p>
     </div>
   )
 }
