898e99a6475497ccf20e3de82734a48fa79fc458
riley soloner <rileysoloner@gmail.com>
2022-11-17 13:12:11 -0500

    nice letter riley

--- src/components/MoreDrawer.js
+++ src/components/MoreDrawer.js
@@ -27,14 +27,16 @@ const MoreDrawer = () => {
 
             <div className='drawerWords1'>
               <p>Hey! Thank you for clicking "more." I appreciate it. <br /> <br /> 
-              I'm going to level with you. I'm new to software engineering. I'm a "career shifter." <br />
-              I was quite successful in my last career, but finding and keeping jobs in entertainment is kind of like a lottery. <br />
+              I'm going to level with you. I'm new to software engineering. I'm a career shifter. <br />
+              I was successful in my entertainment career, but finding and keeping jobs in that field is a bit too much like a lottery. <br /><br />
               My technical skills are growing rapidly as I navigate my post-bootcamp life. I'm a trainable, fast learning, dedicated guy. <br />
-              I'm also a creative, outgoing and kind human being interested in amplifying the human element in workplaces. <br />
-              I mobilized my bootcamp cohort so we could collectively demand better learning conditions. <br />
-              Come presentation day, my technology may not be the most advanced. But I'll be sure to present and communicate in a way that will make an impression. <br />
-              I naturally gravitate towards communicative leadership roles. 
-
+              Given my past experience, I'm a creative, outgoing and kind human being that values the human element in workplaces. <br /><br />
+              Through no conscious effort, I naturally fell into a role on campus as someone with whom my classmates could have a sincere, encouraging conversation.<br /><br />
+              When our bootcam's cost-saving, corner cutting tactics felt like a total rug-pull on the student body, I mobilized my cohort so we could collectively (successfully) demand better learning conditions. <br /><br />
+              Come presentation day, my technology may not be the most advanced. But I'll be sure to present, communicate and connect in a way that will make an impression. <br />
+              Thank you for reading.
+                <br /><br />
+                Riley 
               </p>
             </div>
             
