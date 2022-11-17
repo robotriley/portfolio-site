256766468ee80b19ed40f2d49b4dfb526fd6ceea
riley soloner <rileysoloner@gmail.com>
2022-11-17 14:11:10 -0500

    ok

--- src/components/MoreDrawer.js
+++ src/components/MoreDrawer.js
@@ -20,20 +20,19 @@ const MoreDrawer = () => {
         onClose={() => setOpen(false)}>
         <Drawer.Header id="aboutHeader" >
           <h2>More About Riley</h2>
-          {/* <Button appearance="ghost" color="yellow">show me an example</Button> */}
         </Drawer.Header>
         <Drawer.Body>
           <div>
 
             <div className='drawerWords1'>
               <h5>Hey! Thank you for clicking "more." I appreciate it. <br /> <br /> 
-              Can I be real? I'm new to software engineering (obviously). I'm a career shifter. <br />
+              Can I be real? I'm a new software engineer (obviously). I'm a career shifter. <br />
               I was successful in my entertainment career, but finding and keeping jobs in that field is a bit too much like a lottery. <br /><br />
               My technical skills are growing rapidly as I navigate my post-bootcamp life. I'm a trainable, fast learning, dedicated guy. <br />
               I value and prioritize creative, outgoing and kind human interaction. I seek out and appreciate the human element in workplaces. <br /><br />
               Through no conscious effort, I naturally fell into a role on campus as someone with whom my classmates could have a sincere, encouraging conversation.<br /><br />
-              When our bootcam's cost-saving, corner cutting tactics felt like a total rug-pull on the student body, I mobilized my cohort so we could collectively demand (and successfully get) better learning conditions. <br /><br />
-              Come presentation day, my technology may not be the most advanced. But I'll be sure to present, communicate and connect in a way that will make an impression. <br />
+              When our bootcam's cost-saving, corner cutting tactics felt like a total rug-pull on the student body, I mobilized my cohort so we could collectively demand (and successfully receive) better learning conditions. <br /><br />
+              While my projects may not be the most advanced, I'll be sure to present, communicate and connect in a way that will make an impression. <br />
               Thank you for reading.
                 <br /><br />
                 Riley 
@@ -45,10 +44,12 @@ const MoreDrawer = () => {
       </Drawer>
 
       <Button
-        appearance="link"
+        appearance="primary"
         size='lg'
-        color="red"
-        onClick={() => handleOpen('bottom')}>More</Button>
+        color="violet"
+        onClick={() => handleOpen('bottom')}>
+          More
+      </Button>
     </div>
   )
 }
