76f3cf5ea48b18e08cc38d10762b49b090ae62ec
riley soloner <rileysoloner@gmail.com>
2022-11-22 18:53:41 -0500

    button style

--- src/components/CarouselComponent.js
+++ src/components/CarouselComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.css';
-import { Carousel} from 'rsuite'
+import { Carousel, Button} from 'rsuite'
 import StoryOutlinerDrawer from './StoryOutlinerDrawer';
 import TrollSimulatorDrawer from './TrollSimulatorDrawer';
 import ReviewHaikuDrawer from './ReviewHaikuDrawer';
@@ -15,8 +15,15 @@ const CarouselComponent = () => {
         <div className='carouselSlide'>
           <h1>STORY OUTLINER </h1>
           <h3>streamline your story writing process</h3>
-          <iframe width="760" height="428" src="https://www.youtube.com/embed/U5DdcyA72Y0?start=2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='allowfullscreen'></iframe>
-          {/* <StoryOutlinerDrawer /> */}
+          <iframe width="560" height="428" src="https://www.youtube.com/embed/U5DdcyA72Y0?start=2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='allowfullscreen'></iframe>
+          <Button color="blue"
+                  appearance='primary'
+                  size="lg"
+                  target="_blank"
+                  href="https://github.com/robotriley/story-outliner">
+            <ion-icon name="logo-github"></ion-icon>
+            Github
+          </Button>
         </div>
         <div className='carouselSlide'>
           <h1>REVIEW HAIKUS <ReviewHaikuDrawer /></h1>
@@ -24,8 +31,15 @@ const CarouselComponent = () => {
         <div className='carouselSlide'>
           <h1>TROLL SIMULATOR</h1>
           <h3>choose your troll adventure</h3>
-          <iframe width="760" height="428" src="https://www.youtube.com/embed/Y2jxvthfZIs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='allowfullscreen'></iframe>
-          {/* <TrollSimulatorDrawer /> */}
+          <iframe width="560" height="428" src="https://www.youtube.com/embed/Y2jxvthfZIs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='allowfullscreen'></iframe>
+          <Button color="blue"
+                  appearance='primary'
+                  size='lg'
+                  target="_blank"
+                  href="https://github.com/robotriley/troll-builder">
+            <ion-icon name="logo-github"></ion-icon>
+            Github
+          </Button>
         </div>
       </Carousel>
     </div>
