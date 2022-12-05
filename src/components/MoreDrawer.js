c093d37be11b477954b088759335507456b733ef
riley soloner <rileysoloner@gmail.com>
2022-12-05 14:34:31 -0500

    more drawer edit

--- src/components/MoreDrawer.js
+++ src/components/MoreDrawer.js
@@ -26,12 +26,12 @@ const MoreDrawer = () => {
 
             <div className='drawerWords1'>
               <h5>Hey! Thank you for clicking "more." I appreciate it. <br /> <br /> 
-              Can I be real? I'm a new software engineer (obviously). I'm a career shifter. <br />
+              Can I be real? I'm a new software engineer. I'm a career shifter. <br />
               I was successful in my entertainment career, but finding and keeping jobs in that field is like a lottery. <br /><br />
               My technical skills are growing rapidly as I navigate my post-bootcamp life. I'm a trainable, fast learning, dedicated guy. <br />
               I value and prioritize creative, outgoing and kind human interaction. I seek out and appreciate the human element in workplaces. <br /><br />
               Through no conscious effort, I naturally fell into a role on campus as someone with whom my classmates could have a sincere, encouraging conversation.<br /><br />
-              When our bootcamp's cost-saving, corner cutting tactics felt like a total rug-pull on the student body, I mobilized my cohort so we could collectively demand (and successfully receive) better learning conditions. <br /><br />
+              
               I present, communicate and connect in a way that will make an impression. <br />
               Thank you for reading.
                 <br /><br />
